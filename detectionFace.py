ae8874e0c604f378760caea108c72d289422eaf1
nottalk0922 <pmu42094@naver.com>
2022-11-29 11:15:51 +0900

    Update detectionFace.py
    주석 추가

--- detectionFace.py
+++ detectionFace.py
@@ -1,12 +1,22 @@
 import cv2
 import numpy as np
 
+#Loading haar cascade
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
+# Set camera
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,320) 
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,200)
+
+
+def detectionSleep(img):
+        
+    eyes = eye_cascade.detectMultiScale(img)
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
 
 while True:
     ret, img = cap.read()
@@ -22,9 +32,10 @@ while True:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        cv2.imshow("roi_gray",roi_gray)
+        
+        detectionSleep(roi_gray)
+    
     cv2.imshow('img',img)      
       
     if cv2.waitKey(1) & 0xFF == ord('q'):
